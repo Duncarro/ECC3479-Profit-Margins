609ea57e250f98519d551cf2560789f43be8950b
Kye Duncan <kdun0020@student.monash.edu>
2026-03-31 10:35:23 +1100

    added files to repo

---
+++ src/EDA.R
@@ -0,0 +1,110 @@
+#EDA SCRIPT
+
+#Time series
+library(scales)
+
+p1<-gop |>
+  ggplot(aes(x = date, y = margin)) +
+  geom_line(linewidth = 0.9, colour = "darkorange") +
+  labs(
+    title = "Gross Operating Profit Margin",
+    subtitle = "All industries",
+    x = NULL,
+    y = "Per cent"
+  ) +
+  scale_y_continuous(labels = number_format(accuracy = 0.1)) +
+  theme_classic(base_size = 12) +
+  theme(
+    plot.title = element_text(face = "bold", size = 14),
+    plot.subtitle = element_text(size = 11, margin = margin(b = 8)),
+    axis.title.y = element_text(face = "bold"),
+    axis.text = element_text(colour = "black"),
+    axis.line = element_line(colour = "black"),
+    axis.ticks = element_line(colour = "black"),
+    panel.grid.major.y = element_line(colour = "grey80", linewidth = 0.3),
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor = element_blank()
+  )
+
+ggsave("output/gop_margin.png", plot = p1, width = 8, height = 5, dpi = 300)
+
+
+
+p2<-gos |>
+  ggplot(aes(x = date, y = margin)) +
+  geom_line(linewidth = 0.9, colour = "coral2") +
+  labs(
+    title = "Gross Operating Surplus Margin",
+    subtitle = "All industries",
+    x = NULL,
+    y = "Per cent"
+  ) +
+  scale_y_continuous(labels = number_format(accuracy = 0.1)) +
+  theme_classic(base_size = 12) +
+  theme(
+    plot.title = element_text(face = "bold", size = 14),
+    plot.subtitle = element_text(size = 11, margin = margin(b = 8)),
+    axis.title.y = element_text(face = "bold"),
+    axis.text = element_text(colour = "black"),
+    axis.line = element_line(colour = "black"),
+    axis.ticks = element_line(colour = "black"),
+    panel.grid.major.y = element_line(colour = "grey80", linewidth = 0.3),
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor = element_blank()
+  )
+
+ggsave("output/gos_margin.png", plot = p2, width = 8, height = 5, dpi = 300)
+
+
+
+
+p2<-thw |>
+  ggplot(aes(x = date, y = value)) +
+  geom_line(linewidth = 0.9, colour = "cyan3") +
+  labs(
+    title = "Total Hours Worked",
+    x = NULL,
+    y = "UNITTTTTTTT"
+  ) +
+  scale_y_continuous(labels = number_format(accuracy = 0.1)) +
+  theme_classic(base_size = 12) +
+  theme(
+    plot.title = element_text(face = "bold", size = 14),
+    plot.subtitle = element_text(size = 11, margin = margin(b = 8)),
+    axis.title.y = element_text(face = "bold"),
+    axis.text = element_text(colour = "black"),
+    axis.line = element_line(colour = "black"),
+    axis.ticks = element_line(colour = "black"),
+    panel.grid.major.y = element_line(colour = "grey80", linewidth = 0.3),
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor = element_blank()
+  )
+
+ggsave("output/thw_level.png", plot = p2, width = 8, height = 5, dpi = 300)
+
+
+
+
+p2<-gdp |>
+  ggplot(aes(x = date, y = value)) +
+  geom_line(linewidth = 0.9, colour = "darkolivegreen1") +
+  labs(
+    title = "Gross Domestic Product",
+    x = NULL,
+    y = "Millions ($)"
+  ) +
+  scale_y_continuous(labels = number_format(accuracy = 0.1)) +
+  theme_classic(base_size = 12) +
+  theme(
+    plot.title = element_text(face = "bold", size = 14),
+    plot.subtitle = element_text(size = 11, margin = margin(b = 8)),
+    axis.title.y = element_text(face = "bold"),
+    axis.text = element_text(colour = "black"),
+    axis.line = element_line(colour = "black"),
+    axis.ticks = element_line(colour = "black"),
+    panel.grid.major.y = element_line(colour = "grey80", linewidth = 0.3),
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor = element_blank()
+  )
+
+ggsave("output/gdp_level.png", plot = p2, width = 8, height = 5, dpi = 300)
---
+++ src/data_cleaning.R
@@ -0,0 +1,124 @@
+#This file imports the necessary series and tidies them
+#ALL SERIES ARE SEASONALLY ADJUSTED
+
+library(tidyverse)
+library(readabs)
+Sys.setenv(R_READABS_PATH = "data/raw")
+
+#############################################
+# Profit Margin Measures                    #
+#############################################
+
+gop <- read_abs("5676.0")
+#hi
+gop <- gop |>
+  filter(table_no == "56760011", series_type == "Seasonally Adjusted") |>
+  separate(
+    series,
+    into = c("drop1", "drop2", "industry", "drop3", "drop4", "drop5"),
+    sep = " ; "
+  ) |> 
+  select(date, industry, value)
+
+gop_wide <- gop %>%
+  pivot_wider(
+    names_from = industry,
+    values_from = value
+  ) |> 
+  rename(Total = ` Total (Industry)`)
+
+########### SALES DATA
+sales <- read_abs("5676.0")
+
+sales <- sales |> 
+  filter(table_no == "5676006", series_type == "Seasonally Adjusted") |> 
+  select(date, series, value)
+
+sales_wide <- sales |> 
+  pivot_wider(
+    names_from = series,
+    values_from = value
+  ) |> 
+  filter(date > as.Date("2002-09-01")) 
+
+
+sales_wide <- sales_wide |> 
+  mutate(
+    total = rowSums(across(-date), na.rm = TRUE),
+    total_ex_mining = rowSums(
+      across(-c(
+        date,
+        `Sales ;  Total (State) ;  Mining ;  Current Price ;  TOTAL (SCP_SCOPE) ;`
+      )),
+      na.rm = TRUE
+    )) #|> select(date, total, total_ex_mining)
+
+
+##MERGING GOP AND SALES TO MAKE MARGIN
+#GOP MARGIN = GOP / SALES
+
+gop <- left_join(gop_wide, sales_wide,
+          by = "date") |> 
+  select(date, gop = "Total", sales = "total") |> ##WIP 
+  mutate(margin = gop / sales*100) |> #UNIT MISMATCH!!!
+  #SALES DATA VALUES DIFFER FROM EXCEL SHEET.
+  filter(date > as.Date("2002-09-01")) 
+
+
+
+
+##IMPORTING GOS AND GVA
+gos <- read_abs("5206.0")
+
+ gos <- gos |> 
+  filter(series == "All sectors ;  Gross operating surplus ;",
+         series_type == "Seasonally Adjusted") |> 
+   select(date, gos = value) |> 
+   filter(date > as.Date("2002-09-01"))
+ 
+
+ ##GVA
+ gva <- read_abs("5206.0")
+ 
+ gva <- gva |> 
+   filter(series == "Total all industries ;  Gross value added at basic prices ;",
+          series_type == "Seasonally Adjusted") |> 
+   select(date, gva = value) |> 
+   filter(date > as.Date("2002-09-01"))
+ 
+ 
+ ##MERGING GOS AND GVA TO MAKE MARGIN
+ #GOP MARGIN = GOS / GVA
+ 
+gos <- left_join(gos, gva,
+                  by = "date") |> 
+   select(date, gos, gva) |> 
+   mutate(margin = gos / gva*100) |>  
+   filter(date > as.Date("2002-09-01")) 
+  
+
+#############################################
+# Business Cycle Indictators                #
+#############################################
+  
+##THW
+thw <- read_abs("6202.0")
+
+thw <- thw |> 
+  filter(series_id == "A84426298K", date > as.Date("2002-09-01")) |> 
+  select(date, value) 
+
+
+##GDP (chain volume)
+gdp <- read_abs("5206.0")
+
+gdp <- gdp |> 
+  filter(series_id == "A2304402X", date > as.Date("2002-09-01")) |> 
+  select(date, value) 
+  
+
+###EXPORTING CLEANED DATA
+write_csv(gop, "data/clean/gop.csv")
+write_csv(gos, "data/clean/gos.csv")
+write_csv(thw, "data/clean/thw.csv")
+write_csv(gdp, "data/clean/gdp.csv")
